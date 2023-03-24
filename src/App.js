28b71d108caf7c05ab79b6798a8c002277e2086c
Nicolas Torre <nicolas.torre@innew.la>
2023-03-24 09:09:25 -0300

    true

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import React from "react";
-import "./App.css";
-import Menu from "./Menu";
-import Rambo from "./Rambo";
-/* function Suma(x, y, p) {
-  return x + y - p;
-}
+import { useEffect, useState } from "react";
 
-let name = "Fabricio"
-let subname = "Torre" */
-
-const testScript = {};
-
+const CAT_ENDEPONT_RANDOM_FACT = "https://catfact.ninja/fact";
+const URL_IMAGE = "https://cataas.com/";
 function App() {
+  const [fact, setFact] = useState();
+  const [imgUrl, setImgUrl] = useState();
+
+  useEffect(() => {
+    fetch(CAT_ENDEPONT_RANDOM_FACT)
+      .then((res) => res.json())
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+
+        const firstWord = fact.split(" ").slice(0, 3).join(" ");
+
+        fetch(
+          `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+        )
+          .then((res) => res.json())
+          .then((response) => {
+            const { url } = response;
+            setImgUrl(URL_IMAGE + url); 
+          });
+      });
+  }, []);
+
   return (
-    <div className="App">
-     <header><Menu/></header>
-     <Rambo />
-    </div>
+    <main>
+      <h1>App de gatitos</h1>
+      {fact && <p>{fact}</p>}
+      {imgUrl && <img src={imgUrl} />}
+    </main>
   );
 }
 
